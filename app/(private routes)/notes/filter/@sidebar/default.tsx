90236f1c6c436d24c50bca2486a8156acc80b471
Skaleivt <skaleiviktor@gmail.com>
2026-03-30 10:26:09 +0300

    Initial commit

---
+++ app/(private routes)/notes/filter/@sidebar/default.tsx
@@ -0,0 +1,31 @@
+import css from "./SidebarNotes.module.css";
+import Link from "next/link";
+
+const tags = ["All", "Work", "Personal", "Meeting", "Shopping", "Todo"];
+
+export default function SidebarNotes() {
+  return (
+    <>
+      <Link
+        className={`${css.menuLink} ${css.menuLinkCreate}`}
+        href="/notes/action/create"
+      >
+        Create note
+      </Link>
+      <ul className={css.menuList}>
+        {tags.map((tag) => (
+          <li key={tag} className={css.menuItem}>
+            <Link
+              href={
+                tag === "All" ? "/notes/filter/All" : `/notes/filter/${tag}`
+              }
+              className={css.menuLink}
+            >
+              {tag}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
